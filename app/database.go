819ffe76f6f0542bfbc85dba886087e024ff3d12
projekbrillylutfan <projekbrillylutfan@gmail.com>
2024-11-07 16:03:07 +0700

    mantap juga nih migration

---
+++ app/database.go
@@ -0,0 +1,28 @@
+package app
+
+import (
+	"database/sql"
+	"programmerzamannow/belajar-golang-restful-api/helper"
+	"time"
+)
+
+func NewDB() *sql.DB {
+	db, err := sql.Open("mysql", "root@tcp(localhost:3306)/belajar_golang_database_migration")
+	helper.PanicIfError(err)
+
+	db.SetMaxIdleConns(5)
+	db.SetMaxOpenConns(20)
+	db.SetConnMaxLifetime(60 * time.Minute)
+	db.SetConnMaxIdleTime(10 * time.Minute)
+
+	return db
+
+	// migrate create -ext sql -dir db/migrations create_table_first
+	// migrate create -ext sql -dir db/migrations create_table_second
+	// migrate create -ext sql -dir db/migrations create_table_third
+	// migrate create -ext sql -dir db/migrations sample_dirty_state
+	// migrate -database "mysql://root:admin@tcp(localhost:3306)/belajar_golang_database_migration" -path db/migrations up
+	// migrate -database "mysql://root:admin@tcp(localhost:3306)/belajar_golang_database_migration" -path db/migrations down
+	// migrate -database "mysql://root:admin@tcp(localhost:3306)/belajar_golang_database_migration" -path db/migrations version
+	// migrate -database "mysql://root:admin@tcp(localhost:3306)/belajar_golang_database_migration" -path db/migrations force 20220922043738
+}
---
+++ db/migrations/20241107084334_create_table_category.up.sql
@@ -0,0 +1,5 @@
+create Table category (
+    id int NOT NULL AUTO_INCREMENT,
+    name varchar(255) NOT NULL,
+    PRIMARY KEY (id)
+) engine=InnoDB;
